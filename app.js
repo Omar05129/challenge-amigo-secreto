addbebd3ce30b88b7908b0dc34fc7e6c2137006c
Omar Perez <151819@unsaac.edu.pe>
2025-08-25 13:25:50 -0500

    Implementa funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos =[]; //Array para almacenar los nombres
+
+//Función para agregar amigos
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value.trim();
+    //console.log(amigo);
+    if (amigo =="") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(amigo);
+        console.log(amigos);
+    }
+    limpiarcaja();
+}
+
+//Función para limpiar caja
+function limpiarcaja(){
+    document.querySelector("#amigo").value="";
+}
