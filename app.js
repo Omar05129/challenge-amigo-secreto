abe12ed628ac81500ee3cf48e12730aaa4e8348f
Omar Perez <151819@unsaac.edu.pe>
2025-08-25 14:13:57 -0500

    Muestra los amigos en pantalla

--- app.js
+++ app.js
@@ -12,9 +12,25 @@ function agregarAmigo() {
         console.log(amigos);
     }
     limpiarcaja();
+    asignarTextoElemento("listaAmigos", amigos);
 }
 
 //Función para limpiar caja
 function limpiarcaja(){
     document.querySelector("#amigo").value="";
+}
+
+//Función asignar texto elemento
+function asignarTextoElemento(ID, array){
+    //Obtener el elemento de la lista
+    let lista = document.getElementById(ID);
+    //Limpiar la lista existente
+    lista.innerHTML="";
+    //Agregar elementos a la lista
+    for (let i = 0; i < array.length; i++) {
+        let li = document.createElement("li"); // crear un <li>
+        li.textContent = array[i];             // ponerle el nombre del amigo
+        lista.appendChild(li);                 // añadirlo al <ul>
+    }
+    
 }
